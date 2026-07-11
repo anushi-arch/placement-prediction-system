55a1661400de25e6f18ae6460f35315a2502ae79
Anushika <2006anushika@gmail.com>
2026-07-11 19:29:39 +0530

    Connected project to Railway MySQL

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 import mysql.connector
+import streamlit as st
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="placement_ai"
+        host=st.secrets["DB_HOST"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"],
+        port=int(st.secrets["DB_PORT"])
     )
 def save_prediction(student_name, prediction, probability, model_used):
     conn = get_connection()
